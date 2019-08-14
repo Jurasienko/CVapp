a175edc60e10b3d4d8727a8465e9dcbbf43913e8
Mateusz Jur <mateusz.jur92@gmail.com>
2019-08-14 15:05:35 +0200

    cv controllerviev

--- CVapp/CVapp/ViewController.swift
+++ CVapp/CVapp/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var backround1: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startButton(_ sender: Any) {
+    }
+    @IBAction func CVnextbutton(_ sender: Any) {
+    }
+    @IBAction func unwindFromCv2VC(unwindSegue: UIStoryboardSegue) {
+    
 }
 
+}
