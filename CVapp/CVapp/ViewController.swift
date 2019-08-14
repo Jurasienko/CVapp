1f7f02b0fe3151914abb7c83b749ce7e0787685c
Mateusz Jur <mateusz.jur92@gmail.com>
2019-08-14 14:23:00 +0200

    gitignore empty app

---
+++ CVapp/CVapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CVapp
+//
+//  Created by Mateusz Jur on 14/08/2019.
+//  Copyright © 2019 Mateusz Jur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
